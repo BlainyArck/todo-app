8b9000f105bfd7affe978205ad1a8c7ef53e672d
Alan Diogo "BlainyArck <BlainyArck@users.noreply.github.com>
2018-07-18 17:02:19 -0300

    Migração para o redux
    Migrando o todo-app para o redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
-export default props => {
+const TodoForm = props => {
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -16,7 +20,7 @@ export default props => {
             <Grid cols='12 9 10'>
                 <input id='description' className='form-control'
                     placeholder='Adicione uma tarefa'
-                    onChange={props.handleChange}
+                    onChange={props.changeDescription}
                     onKeyUp={keyHandler}
                     value={props.description}>
                 </input>
@@ -39,3 +43,7 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
